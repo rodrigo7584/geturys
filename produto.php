f2bba91de41948280e1fc7fef2bb9820deaf9716
rodrigo7584 <rodrigo.gandhi.oliveira@gmail.com>
2025-01-08 21:37:54 -0300

    08

--- produto.php
+++ produto.php
@@ -180,8 +180,119 @@
   <section class="discover">
     <div class="container">
       <div class="row">
-        <h2 class="title-medium">Descubra outros produtos da Geturys</h2>
+        <h2 class="title-medium title-detail">Descubra outros produtos da Geturys</h2>
         <div class="cards">
+          <div class="product-card-big-thumb">
+
+            <div class="thumb">
+              <img src="./img/mais-produtos-1.png" alt="" class="photo">
+            </div>
+
+            <h3 class="product-title">Óculos De Sol Geturys Masculino</h3>
+
+            <div class="score">
+              <div class="stars">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+              </div>
+              <span class="number">(5.0)</span>
+            </div>
+
+            <div class="prices">
+              <div class="method-1">
+                <span class="price">
+                  R$ 149,00
+                </span>
+                <span class="description">
+                  no PIX
+                </span>
+              </div>
+              <span class="method-2">
+                no cartão em até -<span class="installments"> 6x </span>
+                de -
+                <span class="installment-price">R$24,84</span>
+                sem juros
+              </span>
+            </div>
+
+          </div>
+          <div class="product-card-big-thumb">
+
+            <div class="thumb">
+              <img src="./img/mais-produtos-1.png" alt="" class="photo">
+            </div>
+
+            <h3 class="product-title">Óculos De Sol Geturys Masculino</h3>
+
+            <div class="score">
+              <div class="stars">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+              </div>
+              <span class="number">(5.0)</span>
+            </div>
+
+            <div class="prices">
+              <div class="method-1">
+                <span class="price">
+                  R$ 149,00
+                </span>
+                <span class="description">
+                  no PIX
+                </span>
+              </div>
+              <span class="method-2">
+                no cartão em até -<span class="installments"> 6x </span>
+                de -
+                <span class="installment-price">R$24,84</span>
+                sem juros
+              </span>
+            </div>
+
+          </div>
+          <div class="product-card-big-thumb">
+
+            <div class="thumb">
+              <img src="./img/mais-produtos-1.png" alt="" class="photo">
+            </div>
+
+            <h3 class="product-title">Óculos De Sol Geturys Masculino</h3>
+
+            <div class="score">
+              <div class="stars">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+              </div>
+              <span class="number">(5.0)</span>
+            </div>
+
+            <div class="prices">
+              <div class="method-1">
+                <span class="price">
+                  R$ 149,00
+                </span>
+                <span class="description">
+                  no PIX
+                </span>
+              </div>
+              <span class="method-2">
+                no cartão em até -<span class="installments"> 6x </span>
+                de -
+                <span class="installment-price">R$24,84</span>
+                sem juros
+              </span>
+            </div>
+
+          </div>
           <div class="product-card-big-thumb">
 
             <div class="thumb">
@@ -221,6 +332,141 @@
           </div>
         </div>
       </div>
+    </div>
+  </section>
+  <section class="review">
+    <div class="container">
+      <div class="row">
+        <div class="review-top">
+          <div class="review-header">
+            <h2 class="title-medium">Avaliação geral do produto</h2>
+            <button class="btn-review">
+              <img src="./img/star-3.svg" alt="" class="icon">
+              Faça uma avaliação
+            </button>
+          </div>
+          <div class="review-content">
+            <div class="total-score">
+              <span class="value">
+                <span class="percentual-value">5.0</span>/5.0
+              </span>
+              <span class="quantity">
+                Baseado em <span class="quantity-value">240</span> avaliação
+              </span>
+              <div class="stars">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+              </div>
+            </div>
+            <div class="score-filter">
+              <h3 class="score-filter-title">Ordenar por nota</h3>
+              <div class="buttons">
+                <button class="score-filter-button">
+                  <span class="number">5</span>
+                  <div class="bar">
+                    <div class="fill" style="width: 100%;"></div>
+                  </div>
+                </button>
+                <button class="score-filter-button">
+                  <span class="number">4</span>
+                  <div class="bar">
+                    <div class="fill" style="width: 40%;"></div>
+                  </div>
+                </button>
+                <button class="score-filter-button">
+                  <span class="number">3</span>
+                  <div class="bar">
+                    <div class="fill" style="width: 10%;"></div>
+                  </div>
+                </button>
+                <button class="score-filter-button">
+                  <span class="number">2</span>
+                  <div class="bar">
+                    <div class="fill" style="width: 5%;"></div>
+                  </div>
+                </button>
+                <button class="score-filter-button">
+                  <span class="number">1</span>
+                  <div class="bar">
+                    <div class="fill" style="width: 0%;"></div>
+                  </div>
+                </button>
+              </div>
+            </div>
+            <div class="evaluation-filter">
+              <h3 class="evaluation-filter-title">Filtra avaliações</h3>
+              <button class="evaluation-filter-button">
+                <img src="./img/picture-2.svg" alt="" class="icon">
+                Com mídias
+              </button>
+              <button class="evaluation-filter-button">
+                <img src="./img/ballon-2.svg" alt="" class="icon">
+                Com comentários
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="evaluations">
+          <div class="evaluation">
+            <h3 class="name">Marcos Fontes Neto</h3>
+            <div class="date-city">
+              <span class="date">11/12/2024</span>
+              <span class="city">São Paulo - SP</span>
+            </div>
+            <span class="about">Avaliação referente ao item
+              <span class="item"> Óculos de Sol Geturys Masculino</span>
+            </span>
+            <div class="score">
+              <span class="stars-value">
+                5 Estrelas
+              </span>
+              <div class="stars">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+              </div>
+            </div>
+            <span class="evaluation-title">Avaliação do produto</span>
+            <p class="evaluation-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          </div>
+          <div class="evaluation">
+            <h3 class="name">Marcos Fontes Neto</h3>
+            <div class="date-city">
+              <span class="date">11/12/2024</span>
+              <span class="city">São Paulo - SP</span>
+            </div>
+            <span class="about">Avaliação referente ao item
+              <span class="item"> Óculos de Sol Geturys Masculino</span>
+            </span>
+            <div class="score">
+              <span class="stars-value">
+                5 Estrelas
+              </span>
+              <div class="stars">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+                <img src="./img/star-2.svg" alt="" class="icon">
+              </div>
+            </div>
+            <span class="evaluation-title">Avaliação do produto</span>
+            <p class="evaluation-content">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+              took a galley of type and scrambled it to make a type specimen book. It has survived not only five
+              centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+          </div>
+        </div>
+      </div>
+    </div>
     </div>
   </section>
 </main>
