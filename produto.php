5ff4fdd090f3d9d5caad9b382e616fa15a52f208
rodrigo7584 <waboo@waboo.com.br>
2025-01-09 17:10:20 -0300

    09/01/2025

--- produto.php
+++ produto.php
@@ -469,5 +469,55 @@
     </div>
     </div>
   </section>
+  <section class="new-collection">
+    <div class="container">
+      <div class="row">
+        <h2 class="title-thin">NOVA COLEÇÃO</h2>
+        <h3 class="title title-detail">
+          Perfeito para presentear
+        </h3>
+        <div class="cards">
+          <div class="product-card">
+            <button class="btn-like">
+              <img src="./img/heart-2.svg" alt="" class="icon" />
+            </button>
+
+            <div class="product-thumb">
+              <img src="./img/avulso-1.png" alt="" class="product-img" />
+            </div>
+
+            <h3 class="product-title">Óculos De Sol Geturys Masculino</h3>
+
+            <div class="stars-comments">
+              <div class="stars">
+                <img src="./img/star.svg" alt="" class="icon" />
+                <img src="./img/star.svg" alt="" class="icon" />
+                <img src="./img/star.svg" alt="" class="icon" />
+                <img src="./img/star.svg" alt="" class="icon" />
+                <img src="./img/star.svg" alt="" class="icon" />
+              </div>
+              <div class="comments">
+                <img src="./img/ballon.svg" alt="" class="icon" />
+                <span class="score">5.0</span>
+              </div>
+            </div>
+            <div class="price-container">
+              <div class="old-price-discount">
+                <span class="old-price"> R$ 249,00 </span>
+                <span class="discount"> -10% </span>
+              </div>
+              <h4 class="price">R$149,00</h4>
+            </div>
+            <div class="product-card-footer">
+              <button class="btn-buy"> Comprar</button>
+              <button class="btn-chart">
+                <img src="./img/chart.svg" alt="" class="icon" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </main>
 <?php include './src/foot.php'?>
